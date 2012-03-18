dfa2f75d522399750967ea70a677b5c0b940e962
Kasheftin <kasheftin@gmail.com>
2012-03-18 12:25:03 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Here're couple of dummy methods for working mostly with textareas.
+They're formatted as jquery-plugin.
+$("textarea").insertAtCursor("some text"); // inserts "some text" at the cursor-specified position.
+$("textarea").insertAroundCursor("before text","after text"); // inserts "before text" before the selection and "after text" - after it.
+
---
+++ jquery.textarea.caret.js
@@ -0,0 +1,51 @@
+jQuery.fn.extend({
+	insertAtCursor: function(myValue) {
+		return this.each(function(i) {
+			if (document.selection) {
+				this.focus();
+				sel = document.selection.createRange();
+				sel.text = myValue;
+				this.focus();
+			}
+			else if (this.selectionStart || this.selectionStart == "0") {
+				var startPos = this.selectionStart;
+				var endPos = this.selectionEnd;
+				var scrollTop = this.scrollTop;
+				this.value = this.value.substring(0,startPos) + myValue + this.value.substring(endPos,this.value.length);
+				this.focus();
+				this.selectionStart = startPos + myValue.length;
+				this.selectionEnd = startPos + myValue.length;
+				this.scrollTop = scrollTop;
+			} 
+			else {
+				this.value += myValue;
+				this.focus();
+			}
+		});
+	},
+	insertAroundCursor: function(myValueBefore,myValueAfter) {
+		return this.each(function(i) {
+			if (document.selection) {
+				this.focus();
+				sel = document.selection.createRange();
+				sel.text = myValueBefore + sel.text + myValueAfter;
+				this.focus();
+			}
+			else if (this.selectionStart || this.selectionStart == "0") {
+				var startPos = this.selectionStart;
+				var endPos = this.selectionEnd;
+				var scrollTop = this.scrollTop;
+				this.value = this.value.substring(0,startPos) + myValueBefore + this.value.substring(startPos,endPos) + myValueAfter + this.value.substring(endPos,this.value.length);
+				this.focus();
+				this.selectionStart = startPos + myValueBefore.length;
+				this.selectionEnd = endPos + myValueBefore.length;
+				this.scrollTop = scrollTop;
+			}
+			else {
+				this.value += myValueBefore + myValueAfter;
+				this.focus();
+			}
+		});
+	}
+});
+
